1d823a9d9265a19141028aa89142a822c3eda76f
Simon Cox <simon@the-coxes.com>
2015-12-02 08:00:19 -0500

    Install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+script_dir=$(dirname $0)
+for dir in "data" "sessions"; do 
+    rm -rf $script_dir/$dir
+    cp -r /var/www/SecretSanta/SecretSanta/$dir $script_dir/
+done
+sudo rsync -vaz * /var/www/SecretSanta/SecretSanta/
+sudo service apache2 restart
