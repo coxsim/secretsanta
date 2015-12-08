127d840923c98057f58d228b7bbdf8df2b97b2e6
Simon Cox <simon.cox@gmail.com>
2015-12-08 12:35:07 +0800

    chown data ans sessions directory

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ script_dir=$(dirname $0)
 for dir in "data" "sessions"; do 
     rm -rf $script_dir/$dir
     cp -r /var/www/SecretSanta/SecretSanta/$dir $script_dir/
+    sudo chown www-data /var/www/SecretSanta/SecretSanta/$dir
 done
 sudo rsync -vaz * /var/www/SecretSanta/SecretSanta/
 sudo service apache2 restart
